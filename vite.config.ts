352f8d70fa648d8de414a2f613cabf8419114fcb
Violet Hyun <sh4771@columbia.edu>
2025-12-10 12:40:40 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-  import { defineConfig } from 'vite';
-  import react from '@vitejs/plugin-react-swc';
-  import path from 'path';
-
-  export default defineConfig({
-    plugins: [react()],
+export default defineConfig({
+  base: '/passiton_colloquium/',   // 👈 GitHub repo 이름
+  plugins: [react()],
+})
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
